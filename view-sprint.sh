4eacee2ffe58f6b77148ba8f965aca8e6cfe8339
Vince <vince@mod3.tech>
2025-02-28 15:50:33 -0700

    Add jira user to the current user selection

--- view-sprint.sh
+++ view-sprint.sh
@@ -2,7 +2,7 @@
 . ./config.sh
 
 ## Get configured Jira users
-SELECTED_USER=$(gum filter "Current user" "All users" $JIRA_USERS)
+SELECTED_USER=$(gum filter "Current user ($JIRA_USER)" "All users" $JIRA_USERS)
 
 if [[ -z "$SELECTED_USER" ]]; then
     echo "No user selected"
@@ -11,7 +11,7 @@ fi
 
 if [[ "$SELECTED_USER" == "All users" ]]; then
     jira-cli-command sprint list --current
-elif [[ "$SELECTED_USER" == "Current user" ]]; then
+elif [[ "$SELECTED_USER" == "Current user"* ]]; then
     jira-cli-command sprint list --current -a$JIRA_USER
 else
     jira-cli-command sprint list --current -a$SELECTED_USER

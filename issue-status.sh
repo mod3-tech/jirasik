56241c28d00313ebae4300cc7dc010d7d007f6b7
Vince <vince@mod3.tech>
2025-03-04 16:33:26 -0700

    Update JIRA_ISSUE_KEY to JIRA_PROJECT_KEY

--- issue-status.sh
+++ issue-status.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 . ./config.sh
 
-JIRA_ISSUE=$(gum input --placeholder "Enter JIRA issue number ($JIRA_ISSUE_KEY-123)")
+JIRA_ISSUE=$(gum input --placeholder "Enter JIRA issue number ($JIRA_PROJECT_KEY-123)")
 
 if [[ ! -z "$JIRA_ISSUE" ]]; then
     # Loop indefinitely until user cancels
     while true; do
-        echo "Move issue ($JIRA_ISSUE_KEY-$JIRA_ISSUE)"
+        echo "Move issue ($JIRA_PROJECT_KEY-$JIRA_ISSUE)"
 
         # Create a temporary file for output
         temp_file=$(mktemp)
 
         # Run the interactive command and tee its output to both the terminal and the temp file
-        jira-cli-command issue move $JIRA_ISSUE_KEY-$JIRA_ISSUE 2>&1 | tee "$temp_file"
+        jira-cli-command issue move $JIRA_PROJECT_KEY-$JIRA_ISSUE 2>&1 | tee "$temp_file"
         exit_code=${PIPESTATUS[0]}
 
         # Check if exit code is non-zero or output contains errors

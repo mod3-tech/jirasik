f7e873f7d82086c555eaf772f35f73d06a9c6ad7
Vince <vince@mod3.tech>
2025-03-07 15:25:52 -0700

    Update sprint to lookup assignees automatically

--- setup.sh
+++ setup.sh
@@ -62,30 +62,28 @@ if [[ "$JIRA_TOKEN" == *"Key not found"* ]] || [[ -z "$JIRA_TOKEN" ]]; then
 fi
 
 # Check configuration
-if [[ "$JIRA_URL" == *"Key not found"* ]] || [[ -z "$JIRA_URL" ]] || [[ "$JIRA_USER" == *"Key not found"* ]] || [[ -z "$JIRA_USER" ]] || [[ "$JIRA_PROJECT_KEY" == *"Key not found"* ]] || [[ -z "$JIRA_PROJECT_KEY" ]]; then
+if [[ "$JIRA_URL" == *"Key not found"* ]] || [[ -z "$JIRA_URL" ]] ||
+    [[ "$JIRA_USER" == *"Key not found"* ]] || [[ -z "$JIRA_USER" ]] ||
+    [[ "$JIRA_BOARD_ID" == *"Key not found"* ]] || [[ -z "$JIRA_BOARD_ID" ]] ||
+    [[ "$JIRA_PROJECT_KEY" == *"Key not found"* ]] || [[ -z "$JIRA_PROJECT_KEY" ]]; then
+
     echo -e "${RED}jirasik is not yet configured!${NC}"
 
-    if [[ "$JIRA_URL" == *"Key not found"* ]]; then
-        JIRA_URL=""
-    fi
-    JIRA_URL=$(grep 'server:' ~/.config/.jira/.config.yml | awk '{print $2}')
+    JIRA_URL=$(grep 'server:' "$JIRACLI_CONFIG_FILE" | awk '{print $2}')
     skate set "$SKATE_KEY_JIRA_URL"@"$SKATE_DB" "$JIRA_URL"
 
-    if [[ "$JIRA_USER" == *"Key not found"* ]]; then
-        JIRA_USER=""
-    fi
-    JIRA_USER=$(grep 'login:' ~/.config/.jira/.config.yml | awk '{print $2}')
+    JIRA_USER=$(grep 'login:' "$JIRACLI_CONFIG_FILE" | awk '{print $2}')
     skate set "$SKATE_KEY_JIRA_USER"@"$SKATE_DB" "$JIRA_USER"
 
-    if [[ "$JIRA_PROJECT_KEY" == *"Key not found"* ]]; then
-        JIRA_PROJECT_KEY=""
-    fi
-    JIRA_PROJECT_KEY=$(grep 'project:' ~/.config/.jira/.config.yml -A 1 | grep 'key:' | awk '{print $2}')
+    JIRA_BOARD_ID=$(grep 'board:' "$JIRACLI_CONFIG_FILE" -A 1 | grep 'id:' | awk '{print $2}')
+    skate set "$SKATE_KEY_JIRA_BOARD_ID"@"$SKATE_DB" "$JIRA_BOARD_ID"
+
+    JIRA_PROJECT_KEY=$(grep 'project:' "$JIRACLI_CONFIG_FILE" -A 1 | grep 'key:' | awk '{print $2}')
     skate set "$SKATE_KEY_JIRA_PROJECT_KEY"@"$SKATE_DB" "$JIRA_PROJECT_KEY"
 
     # If any of these are still empty quit
-    if [[ -z "$JIRA_URL" ]] || [[ -z "$JIRA_USER" ]] || [[ -z "$JIRA_PROJECT_KEY" ]]; then
-        echo -e "${RED}Configuration incomplete. Please fill in all fields.${NC}"
+    if [[ -z "$JIRA_URL" ]] || [[ -z "$JIRA_USER" ]] || [[ -z "$JIRA_BOARD_ID" ]] || [[ -z "$JIRA_PROJECT_KEY" ]]; then
+        echo -e "${RED}Configuration incomplete.${NC}"
         exit 1
     fi
 
